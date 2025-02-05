2a5889676f46a69302301eb44176b002840d17ca
Gunjan7113 <gunjansachdeva2308@gmail.com>
2025-02-05 11:35:45 +0530

    Update calculator - Copy - Copy.cpp

--- calculator - Copy - Copy.cpp
+++ calculator - Copy - Copy.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 int main()
 {
-
-int num1,num2,choice;
-	cout<<"GARV"<<endl;
-	cout<<"2410997105"<<endl;
+	int num1,num2,choice;
 cout<<"Enter the value of num1:-";
 cin>>num1;
 cout<<"Enter the value of num2:-";
@@ -15,9 +12,6 @@ cout<<"Enter 1 for addition\n";
 cout<<"Enter 2 for Subtraction\n";
 cout<<"Enter 3 for Multiplication\n";
 cout<<"Enter 4 for division\n";
-cout<<"Enter 5 for equality\n";
-cout<<"Enter 6 for increment\n";
-cout<<"Enter 7 for decrement\n";
 
 cout<<"Enter your choice please\n";
 cin>>choice;
@@ -36,27 +30,7 @@ switch(choice)
   case 4:
    	  cout<<"Division of 2 numbers is "<<num1/num2;
    	  break;
-case 5:
-       if(num1==num2)
-   	      cout<<"They are equal";
-   	  else
-   	      cout<<"They are not equal";
-   	  break;
-
-case 6:
-     cout<<"Incremented value of num1 is "<<++num1;
-     cout<<"\n Incremented value of num2 is"<<++num2;
-     break;    
-case 7:
-     cout<<"Decremented value of num1 is"<<--num1;
-     cout<<"\n Decremented value of num2 is"<<--num2;
-     break;
-     
      default:
      	cout<<"Kindly check your choice and press b/w 1-7";
-
-
 }
-
-
 }
